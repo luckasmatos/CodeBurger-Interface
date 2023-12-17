9f7d5ccea53910aeb49803c66f2edef7d18d398b
luckasmatos <luckas.matos@hotmail.com>
2023-12-17 11:23:22 -0300

    first commit

--- src/components/toogleMenu/styles.js
+++ src/components/toogleMenu/styles.js
@@ -25,7 +25,7 @@ export const MenuContent = styled.div`
   position: absolute;
   top: -25px;
   left: ${props => (props.open ? '-25px' : '-200vw')};
-  height: calc(100vh-25px);
+  height: 100%;
   background-color: rgba(0, 0, 0, 0.8);
   display: flex;
   flex-direction: column;
