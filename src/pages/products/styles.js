b18e1b7cd2ad9e292598656321e8b434fac5f433
luckasmatos <luckas.matos@hotmail.com>
2023-12-17 18:13:53 -0300

    first commit

--- src/pages/products/styles.js
+++ src/pages/products/styles.js
@@ -7,6 +7,7 @@ export const Container = styled.article`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    overflow: hidden;
 `
 
 export const HomeImg = styled.img`
